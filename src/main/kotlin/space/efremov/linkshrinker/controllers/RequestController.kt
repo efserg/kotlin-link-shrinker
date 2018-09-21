2f52521cfbdb2f78c1eed7f8a674d9b86c25cb8b
Sergey Y. Efremov <mynog@yandex.ru>
2018-09-21 18:51:30 +0300

    Update redirect controller

--- src/main/kotlin/space/efremov/linkshrinker/controllers/RequestController.kt
+++ src/main/kotlin/space/efremov/linkshrinker/controllers/RequestController.kt
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import javax.servlet.http.HttpServletResponse
+import javax.servlet.http.HttpServletResponse.*
 
 @Controller
 @RequestMapping("/{key}")
@@ -11,7 +12,15 @@ class RequestController {
 
     @RequestMapping()
     fun redirect(@PathVariable("key") key: String, response: HttpServletResponse) {
-        response.setHeader("Location", "http://www.eveonline.com")
-        response.status = 302
+        if (key == "aAbBcCdD") {
+            response.setHeader(HEADER_NAME, "http://www.eveonline.com")
+            response.status = SC_MOVED_TEMPORARILY
+        } else {
+            response.status = SC_NOT_FOUND
+        }
+    }
+
+    companion object {
+        private val HEADER_NAME = "Location"
     }
 }
--- src/test/kotlin/space/efremov/linkshrinker/controllers/RedirectControllerTest.kt
+++ src/test/kotlin/space/efremov/linkshrinker/controllers/RedirectControllerTest.kt
@@ -13,6 +13,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
 import space.efremov.linkshrinker.LinkShrinkerApplication
+import javax.servlet.http.HttpServletResponse.SC_MOVED_TEMPORARILY
+import javax.servlet.http.HttpServletResponse.SC_NOT_FOUND
 
 @RunWith(SpringJUnit4ClassRunner::class)
 @SpringBootTest(classes = [LinkShrinkerApplication::class])
@@ -32,15 +34,21 @@ class RedirectControllerTest {
     }
 
     private val PATH = "/aAbBcCdD"
-    private val REDIRECT_STATUS: Int = 302
+    private val BAD_PATH = "/bad_path"
     private val HEADER_NAME = "Location"
     private val HEADER_VALUE = "http://www.eveonline.com"
 
     @Test
     fun controllerMustRedirectUsWhenRequestIsSuccessful() {
         mockMvc.perform(get(PATH))
-                .andExpect(status().`is`(REDIRECT_STATUS))
+                .andExpect(status().`is`(SC_MOVED_TEMPORARILY))
                 .andExpect(header().string(HEADER_NAME, HEADER_VALUE))
+    }
+
+    @Test
+    fun controllerMustReturn404IfBadKey() {
+        mockMvc.perform(get(BAD_PATH))
+                .andExpect(status().`is`(SC_NOT_FOUND))
     }
 
 }
