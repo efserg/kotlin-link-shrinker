c25443f6e21302be39f721334325f4bad723f37e
Sergey Y. Efremov <mynog@yandex.ru>
2018-09-30 18:08:58 +0300

    Update key mapper service for work with DB

--- src/main/kotlin/space/efremov/linkshrinker/services/DefaultKeyMapperService.kt
+++ src/main/kotlin/space/efremov/linkshrinker/services/DefaultKeyMapperService.kt
@@ -2,8 +2,9 @@ package space.efremov.linkshrinker.services
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
-import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.atomic.AtomicLong
+import org.springframework.transaction.annotation.Transactional
+import space.efremov.linkshrinker.model.Link
+import space.efremov.linkshrinker.repositories.LinkRepository
 
 @Component
 class DefaultKeyMapperService : KeyMapperService {
@@ -11,24 +12,21 @@ class DefaultKeyMapperService : KeyMapperService {
     @Autowired
     lateinit var converter: KeyConverterService
 
-    val sequence = AtomicLong(10_000_000L)
+    @Autowired
+    lateinit var repo: LinkRepository
 
-    private val map: MutableMap<Long, String> = ConcurrentHashMap()
-
-    override fun add(link: String): String {
-        val id = sequence.getAndIncrement()
-        val key = converter.idToKey(id)
-        map[id] = link
-        return key
-    }
+    @Transactional
+    override fun add(link: String): String =
+            converter.idToKey(repo.save(Link(link)).id)
 
     override fun getLink(key: String): KeyMapperService.Get {
-        val id = converter.keyToId(key)
-        return if (map.contains(id)) {
-            KeyMapperService.Get.Link(map[id]!!)
-        } else {
+
+        var result: Link? = repo.findById(converter.keyToId(key))
+
+        return if (result != null)
+            KeyMapperService.Get.Link(result.text)
+        else
             KeyMapperService.Get.NotFound(key)
-        }
     }
 
 }
--- src/test/kotlin/space/efremov/linkshrinker/services/DefaultKeyMapperServiceTest.kt
+++ src/test/kotlin/space/efremov/linkshrinker/services/DefaultKeyMapperServiceTest.kt
@@ -6,7 +6,10 @@ import org.junit.Before
 import org.junit.Test
 import org.mockito.InjectMocks
 import org.mockito.Mock
+import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
+import space.efremov.linkshrinker.model.Link
+import space.efremov.linkshrinker.repositories.LinkRepository
 import space.efremov.linkshrinker.whenever
 
 class DefaultKeyMapperServiceTest {
@@ -17,13 +20,23 @@ class DefaultKeyMapperServiceTest {
     @Mock
     lateinit var converter: KeyConverterService
 
+    @Mock
+    lateinit var repo: LinkRepository
+
     @Before
     fun init() {
         MockitoAnnotations.initMocks(this)
+
         whenever(converter.keyToId(KEY_A)).thenReturn(ID_A)
         whenever(converter.idToKey(ID_A)).thenReturn(KEY_A)
         whenever(converter.keyToId(KEY_B)).thenReturn(ID_B)
         whenever(converter.idToKey(ID_B)).thenReturn(KEY_B)
+
+        whenever(repo.findById(Mockito.any())).thenReturn(null)
+        whenever(repo.save(Link(LINK_A))).thenReturn(LINK_OBJECT_A)
+        whenever(repo.save(Link(LINK_B))).thenReturn(LINK_OBJECT_B)
+        whenever(repo.findById(ID_A)).thenReturn(LINK_OBJECT_A)
+        whenever(repo.findById(ID_B)).thenReturn(LINK_OBJECT_B)
     }
 
     private val KEY = "aAbBcCdD"
@@ -33,6 +46,8 @@ class DefaultKeyMapperServiceTest {
     private val ID_A = 10_000_000L
     private val KEY_B = "cde"
     private val ID_B = 10_000_001L
+    private val LINK_OBJECT_A: Link = Link(LINK_A, ID_A)
+    private val LINK_OBJECT_B: Link = Link(LINK_B, ID_B)
 
     @Test
     fun clientCannotAddLink() {
